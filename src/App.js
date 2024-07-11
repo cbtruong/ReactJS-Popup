95db060276afb8a7501c5e0947b4cf1246b1b686
cbtruong <batruong12122002@gmail.com>
2024-07-11 17:45:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const popupRef=useRef();
+  const handleClosePopup=()=>{
+    popupRef.current.style.zIndex = "-1";
+    popupRef.current.style.opacity = "0";
+  }
+  const handleOpenPopup=()=>{
+    popupRef.current.style.zIndex = "999";
+    popupRef.current.style.opacity = "1";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="popups">
+      <button onClick={handleOpenPopup}>Submit</button>
+      <div className="popup" ref={popupRef}>
+        <div className="popup_wrapper">
+          <i class='bx bxs-user-circle popup_icon'></i>
+          <h1>Thank You!</h1>
+          <p>Your detail has been successfully submitted</p>
+          <button onClick={handleClosePopup}>OK</button>
+        </div>
+      </div>
     </div>
   );
 }
